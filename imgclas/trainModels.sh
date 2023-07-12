f9084fbfb991655305e89868250784694356b356
Pablo-Izquierdo <pabloizquierdogonzalezuc@gmail.com>
2023-07-12 11:58:15 +0000

    Worked at notebooks early stoping patient set a bit higher

--- imgclas/trainModels.sh
+++ imgclas/trainModels.sh
@@ -14,7 +14,12 @@ echo "Running config-b8-lr01.yaml"
 python3 train_runfile.py config-b8-lr01.yaml
 COMMENT-EXAMPLE
 
-echo "Running config-b4-lr001-l20005.yaml"
-python3 train_runfile.py config-b4-lr001-l20005.yaml
-echo "Running config-b4-lr001-l2001.yaml"
-python3 train_runfile.py config-b4-lr001-l2001.yaml
+#echo "Running config-b4-lr001-l20005.yaml"
+#python3 train_runfile.py config-b4-lr001-l20005.yaml
+#echo "Running config-b4-lr001-l2001.yaml"
+#python3 train_runfile.py config-b4-lr001-l2001.yaml
+
+echo "Running config-b4-lr01.yaml"
+python3 train_runfile.py config-b4-lr01.yaml
+echo "Running config-b8-lr001.yaml"
+python3 train_runfile.py config-b8-lr001.yaml

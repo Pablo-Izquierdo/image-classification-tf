c9d084625075d45595a1dc9c55f61e3726f8a262
Pablo-Izquierdo <pabloizquierdogonzalezuc@gmail.com>
2023-11-27 07:31:42 +0000

    Implemented CV, need to test

--- imgclas/train_runfile.py
+++ imgclas/train_runfile.py
@@ -59,84 +59,85 @@ def train_fn(TIMESTAMP, CONF):
     utils.create_dir_tree()
     utils.backup_splits()
 
-    # Load the training data
-    X_train, y_train = load_data_splits(splits_dir=paths.get_ts_splits_dir(),
-                                        im_dir=paths.get_images_dir(),
-                                        split_name='train')
-
-    # Load the validation data
-    if (CONF['training']['use_validation']) and ('val.txt' in os.listdir(paths.get_ts_splits_dir())):
-        X_val, y_val = load_data_splits(splits_dir=paths.get_ts_splits_dir(),
-                                        im_dir=paths.get_images_dir(),
-                                        split_name='val')
-    else:
-        print('No validation data.')
-        X_val, y_val = None, None
-        CONF['training']['use_validation'] = False
-
-    # Load the class names
-#     class_names = load_class_names(splits_dir=paths.get_ts_splits_dir()) ### for classification
-
-    # Update the configuration
-    CONF['model']['preprocess_mode'] = model_utils.model_modes[CONF['model']['modelname']]
-    CONF['training']['batch_size'] = min(CONF['training']['batch_size'], len(X_train))
-
-    # Compute the mean and std RGB values
-    if CONF['dataset']['mean_RGB'] is None:
-        CONF['dataset']['mean_RGB'], CONF['dataset']['std_RGB'] = compute_meanRGB(X_train)
-
-    #Create data generator for train and val sets
-    train_gen = data_sequence(X_train, y_train,
-                              batch_size=CONF['training']['batch_size'],
-#                               num_classes=CONF['model']['num_classes'], ###
-                              im_size=CONF['model']['image_size'],
-                              mean_RGB=CONF['dataset']['mean_RGB'],
-                              std_RGB=CONF['dataset']['std_RGB'],
-                              preprocess_mode=CONF['model']['preprocess_mode'],
-                              aug_params=CONF['augmentation']['train_mode'])
-    train_steps = int((np.ceil(len(X_train)/CONF['training']['batch_size'])))
-
-    if CONF['training']['use_validation']:
-        val_gen = data_sequence(X_val, y_val,
-                                batch_size=CONF['training']['batch_size'],
-#                                 num_classes=CONF['model']['num_classes'], ###
-                                im_size=CONF['model']['image_size'],
-                                mean_RGB=CONF['dataset']['mean_RGB'],
-                                std_RGB=CONF['dataset']['std_RGB'],
-                                preprocess_mode=CONF['model']['preprocess_mode'],
-                                aug_params=CONF['augmentation']['val_mode'])
-        val_steps = int((np.ceil(len(X_val)/CONF['training']['batch_size'])))
-        
-    else:
-        val_gen = None
-        val_steps = None
-
-    # Launch the training
-    t0 = time.time()
-
-    # Create the model and compile it
-    model, base_model = model_utils.create_model(CONF)
-
-    # Get a list of the top layer variables that should not be applied a lr_multiplier
-    base_vars = [var.name for var in base_model.trainable_variables]
-    all_vars = [var.name for var in model.trainable_variables]
-    top_vars = set(all_vars) - set(base_vars)
-    top_vars = list(top_vars)
-
-    # Set trainable layers
-    if CONF['training']['mode'] == 'fast':
-        for layer in base_model.layers:
-            layer.trainable = False
-
-    model.compile(optimizer=customAdam(lr=CONF['training']['initial_lr'],
-                                       amsgrad=True,
-                                       lr_mult=0.1,
-                                       excluded_vars=top_vars
-                                       ),
-                  loss='mean_squared_error',
-                  metrics=['mae']) ### for regression
-
+    
     if not CONF['training']['use_cross_validation']:
+        # Load the training data
+        X_train, y_train = load_data_splits(splits_dir=paths.get_ts_splits_dir(),
+                                            im_dir=paths.get_images_dir(),
+                                            split_name='train')
+
+        # Load the validation data
+        if (CONF['training']['use_validation']) and ('val.txt' in os.listdir(paths.get_ts_splits_dir())):
+            X_val, y_val = load_data_splits(splits_dir=paths.get_ts_splits_dir(),
+                                            im_dir=paths.get_images_dir(),
+                                            split_name='val')
+        else:
+            print('No validation data.')
+            X_val, y_val = None, None
+            CONF['training']['use_validation'] = False
+
+        # Load the class names
+    #     class_names = load_class_names(splits_dir=paths.get_ts_splits_dir()) ### for classification
+
+        # Update the configuration
+        CONF['model']['preprocess_mode'] = model_utils.model_modes[CONF['model']['modelname']]
+        CONF['training']['batch_size'] = min(CONF['training']['batch_size'], len(X_train))
+
+        # Compute the mean and std RGB values
+        if CONF['dataset']['mean_RGB'] is None:
+            CONF['dataset']['mean_RGB'], CONF['dataset']['std_RGB'] = compute_meanRGB(X_train)
+
+        #Create data generator for train and val sets
+        train_gen = data_sequence(X_train, y_train,
+                                  batch_size=CONF['training']['batch_size'],
+    #                               num_classes=CONF['model']['num_classes'], ###
+                                  im_size=CONF['model']['image_size'],
+                                  mean_RGB=CONF['dataset']['mean_RGB'],
+                                  std_RGB=CONF['dataset']['std_RGB'],
+                                  preprocess_mode=CONF['model']['preprocess_mode'],
+                                  aug_params=CONF['augmentation']['train_mode'])
+        train_steps = int((np.ceil(len(X_train)/CONF['training']['batch_size'])))
+
+        if CONF['training']['use_validation']:
+            val_gen = data_sequence(X_val, y_val,
+                                    batch_size=CONF['training']['batch_size'],
+    #                                 num_classes=CONF['model']['num_classes'], ###
+                                    im_size=CONF['model']['image_size'],
+                                    mean_RGB=CONF['dataset']['mean_RGB'],
+                                    std_RGB=CONF['dataset']['std_RGB'],
+                                    preprocess_mode=CONF['model']['preprocess_mode'],
+                                    aug_params=CONF['augmentation']['val_mode'])
+            val_steps = int((np.ceil(len(X_val)/CONF['training']['batch_size'])))
+
+        else:
+            val_gen = None
+            val_steps = None
+
+        # Launch the training
+        t0 = time.time()
+
+        # Create the model and compile it
+        model, base_model = model_utils.create_model(CONF)
+
+        # Get a list of the top layer variables that should not be applied a lr_multiplier
+        base_vars = [var.name for var in base_model.trainable_variables]
+        all_vars = [var.name for var in model.trainable_variables]
+        top_vars = set(all_vars) - set(base_vars)
+        top_vars = list(top_vars)
+
+        # Set trainable layers
+        if CONF['training']['mode'] == 'fast':
+            for layer in base_model.layers:
+                layer.trainable = False
+
+        model.compile(optimizer=customAdam(lr=CONF['training']['initial_lr'],
+                                           amsgrad=True,
+                                           lr_mult=0.1,
+                                           excluded_vars=top_vars
+                                           ),
+                      loss='mean_squared_error',
+                      metrics=['mae']) ### for regression
+
         history = model.fit_generator(generator=train_gen,
                                   steps_per_epoch=train_steps,
                                   epochs=CONF['training']['epochs'],
@@ -146,7 +147,7 @@ def train_fn(TIMESTAMP, CONF):
                                   verbose=1, max_queue_size=5, workers=4,
                                   use_multiprocessing=CONF['training']['use_multiprocessing'],
                                   initial_epoch=0)
-    
+
         # Saving everything
         print('Saving data to {} folder.'.format(paths.get_timestamped_dir()))
         print('Saving training stats ...')
@@ -173,29 +174,103 @@ def train_fn(TIMESTAMP, CONF):
         # print('Saving the model to protobuf...')
         # fpath = os.path.join(paths.get_checkpoints_dir(), 'final_model.proto')
         # model_utils.save_to_pb(model, fpath)
-    
+
     else:
-        # Cross-Validation
-        X, y = split_data_cross_validation(splits_dir=paths.get_ts_splits_dir(),
-                                        im_dir=paths.get_images_dir())
         
-        kwargs = {
-            "TIMESTAMP": TIMESTAMP,
-            "CONF": CONF,
-            "validation_data":None,
-            "validation_steps":None,
-            "callbacks":utils.get_callbacks(CONF),
-            "verbose":1, 
-            "max_queue_size":5,
-            "workers":4,
-            "initial_epoch":0}
+        num_folds = len([x for x in os.listdir(paths.get_ts_splits_dir()) if 'Fold-' in x])
         
-        modelcv = cross_val_model(model)
-        # Evaluate using 10-fold Cross Validation
-        kfold = GroupKFold(n_splits=3)
-        results = cross_val_score(modelcv, X, y, groups=y, cv=kfold, fit_params=kwargs, scoring = 'neg_mean_absolute_error')
-        print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
-    
+        #Create folds directories
+        utils.create_folds_tree(num_folds)
+        for fold_iter in range(0,num_folds):
+            # Load the training data
+            X_train, y_train = load_data_splits(splits_dir=paths.get_ts_splits_dir(),
+                                                im_dir=paths.get_images_dir(),
+                                                split_name=f'Fold-{fold_iter}/train')
+
+             # Update the configuration
+            CONF['model']['preprocess_mode'] = model_utils.model_modes[CONF['model']['modelname']]
+            CONF['training']['batch_size'] = min(CONF['training']['batch_size'], len(X_train))
+
+            # Compute the mean and std RGB values
+            if CONF['dataset']['mean_RGB'] is None:
+                CONF['dataset']['mean_RGB'], CONF['dataset']['std_RGB'] = compute_meanRGB(X_train)
+
+            #Create data generator for train and val sets
+            train_gen = data_sequence(X_train, y_train,
+                                      batch_size=CONF['training']['batch_size'],
+        #                               num_classes=CONF['model']['num_classes'], ###
+                                      im_size=CONF['model']['image_size'],
+                                      mean_RGB=CONF['dataset']['mean_RGB'],
+                                      std_RGB=CONF['dataset']['std_RGB'],
+                                      preprocess_mode=CONF['model']['preprocess_mode'],
+                                      aug_params=CONF['augmentation']['train_mode'])
+            train_steps = int((np.ceil(len(X_train)/CONF['training']['batch_size'])))
+
+            val_gen = None
+            val_steps = None
+
+            # Launch the training
+            t0 = time.time()
+
+            # Create the model and compile it
+            model, base_model = model_utils.create_model(CONF)
+
+            # Get a list of the top layer variables that should not be applied a lr_multiplier
+            base_vars = [var.name for var in base_model.trainable_variables]
+            all_vars = [var.name for var in model.trainable_variables]
+            top_vars = set(all_vars) - set(base_vars)
+            top_vars = list(top_vars)
+
+            # Set trainable layers
+            if CONF['training']['mode'] == 'fast':
+                for layer in base_model.layers:
+                    layer.trainable = False
+
+            model.compile(optimizer=customAdam(lr=CONF['training']['initial_lr'],
+                                               amsgrad=True,
+                                               lr_mult=0.1,
+                                               excluded_vars=top_vars
+                                               ),
+                          loss='mean_squared_error',
+                          metrics=['mae']) ### for regression
+
+            history = model.fit_generator(generator=train_gen,
+                                      steps_per_epoch=train_steps,
+                                      epochs=CONF['training']['epochs'],
+                                      validation_data=val_gen,
+                                      validation_steps=val_steps,
+                                      callbacks=utils.get_callbacks(CONF, fold_iter=fold_iter),
+                                      verbose=1, max_queue_size=5, workers=4,
+                                      use_multiprocessing=CONF['training']['use_multiprocessing'],
+                                      initial_epoch=0)
+
+            # Saving everything
+            print('Saving data to {} folder.'.format(f'{paths.get_timestamped_dir()}/Fold-{fold_iter}'))
+            print('Saving training stats ...')
+            stats = {'epoch': history.epoch,
+                     'training time (s)': round(time.time()-t0, 2),
+                     'timestamp': TIMESTAMP,
+                     'mean RGB pixel': CONF['dataset']['mean_RGB'],
+                     'standard deviation of RGB pixel': CONF['dataset']['std_RGB'],
+                     'batch_size': CONF['training']['batch_size']}
+            stats.update(history.history)
+            stats = json_friendly(stats)
+            stats_dir = paths.get_stats_dir()
+            with open(os.path.join(stats_dir, f'stats_Fold-{fold_iter}.json'), 'w') as outfile:
+                json.dump(stats, outfile, sort_keys=True, indent=4)
+
+            print('Saving the configuration ...')
+            model_utils.save_conf(CONF)
+
+            print('Saving the model to h5...')
+            fpath = os.path.join(paths.get_checkpoints_dir(), 'final_model.h5')
+            model.save(fpath,
+                       include_optimizer=True)
+
+            # print('Saving the model to protobuf...')
+            # fpath = os.path.join(paths.get_checkpoints_dir(), 'final_model.proto')
+            # model_utils.save_to_pb(model, fpath)
+        
 
     print('Finished')
 
